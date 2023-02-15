e6993158fefa5dab0a3f1eabb5e88a6620d016d6
Brandon <chenzhiwei1@forchange.tech>
2023-02-15 11:13:44 +0800

    feat: edit tag

--- base/entity.go
+++ base/entity.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Entity struct {
-	Id             int64 `xorm:"pk autoincr bigint comment('主键/周进/2020-02-02')" json:"id"`
-	CreateBy       int64 `xorm:"bigint not null comment('创建者/周进/2020-02-02')" json:"createBy"`
-	CreateTime     int64 `xorm:"bigint not null comment('创建时间/周进/2020-02-02')" json:"createTime"`
-	LastModifyBy   int64 `xorm:"bigint null COMMENT('记录最后修改者/黄旭龙/2020-02-02')" json:"lastModifyBy"`
-	LastModifyTime int64 `xorm:"bigint null COMMENT('记录最后修改时间/黄旭龙/2020-02-02')" json:"lastModifyTime"`
+	Id             int64 `xorm:"pk autoincr bigint"`
+	CreateBy       int64 `xorm:"bigint not null"`
+	CreateTime     int64 `xorm:"bigint not null"`
+	LastModifyBy   int64 `xorm:"bigint null"`
+	LastModifyTime int64 `xorm:"bigint null"`
 }
 
 func (t *Entity) BeforeInsert() {
